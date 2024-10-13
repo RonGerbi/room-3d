c83d3f74ceb64ab4c1ef306ad8cc35c2feff1c20
RonGerbi <91036274+RonGerbi@users.noreply.github.com>
2024-10-13 19:24:57 +0300

    adjust lightbulb position, start work on light and shadow

--- myOpenGL/cOGL.cs
+++ myOpenGL/cOGL.cs
@@ -258,6 +258,22 @@ namespace OpenGL
 
 
             initRenderingGL();
+
+            // Enable lighting
+            GL.glEnable(GL.GL_LIGHTING); // Enable lighting
+            GL.glEnable(GL.GL_LIGHT0); // Enable light 0
+
+            float[] lightPos = new float[] { 12.5f, 17.0f, 12.5f, 1.0f }; // Light position
+            float[] lightAmbient = new float[] { 0.2f, 0.2f, 0.2f, 1.0f }; // Ambient light
+            float[] lightDiffuse = new float[] { 1.0f, 1.0f, 1.0f, 1.0f }; // Diffuse light
+            float[] lightSpecular = new float[] { 1.0f, 1.0f, 1.0f, 1.0f }; // Specular light
+
+            GL.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, lightPos);
+            GL.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, lightAmbient);
+            GL.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, lightDiffuse);
+            GL.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, lightSpecular);
+
+            GL.glEnable(GL.GL_DEPTH_TEST); // Enable depth testing
         }
 
         public void OnResize()
@@ -751,18 +767,119 @@ namespace OpenGL
 
         private void drawLamp()
         {
+            GL.glPushMatrix();
+            GL.glTranslatef(12.5f, 17f, 12.5f);
+            GLUquadric obj = GLU.gluNewQuadric();
             GL.glEnable(GL.GL_BLEND);
 
             GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
-            GL.glColor4f(1.0f, 1.0f, 1.0f, 0.5f); 
-
-            GLUquadric obj = GLU.gluNewQuadric();
+            GL.glColor4f(1.0f, 1.0f, 1.0f, 0.5f);
 
             GLU.gluSphere(obj, 0.2, 20, 20);
-            GLU.gluDeleteQuadric(obj);
-
             GL.glDisable(GL.GL_BLEND);
+
+            GL.glColor3f(0f, 0f, 0f);
+            GL.glTranslatef(0f, 0.1f, 0f);
+            GL.glRotatef(-90f, 1f, 0f, 0f);
+            GLU.gluCylinder(obj, 0.2, 0.3, 0.6, 20, 20);
+
+            GLU.gluDeleteQuadric(obj);
+            GL.glPopMatrix();
         }
+
+        /*float[] cubeXform = new float[16];
+        private void MakeShadowMatrix(float[,] points)
+        {
+            float[] planeCoeff = new float[4];
+            float dot;
+
+            // Find the plane equation coefficients
+            // Find the first three coefficients the same way we
+            // find a normal.
+            calcNormal(points, planeCoeff);
+
+            // Find the last coefficient by back substitutions
+            planeCoeff[3] = -(
+                (planeCoeff[0] * points[2, 0]) + (planeCoeff[1] * points[2, 1]) +
+                (planeCoeff[2] * points[2, 2]));
+
+
+            // Dot product of plane and light position
+            dot = planeCoeff[0] * pos[0] +
+                    planeCoeff[1] * pos[1] +
+                    planeCoeff[2] * pos[2] +
+                    planeCoeff[3];
+
+            // Now do the projection
+            // First column
+            cubeXform[0] = dot - pos[0] * planeCoeff[0];
+            cubeXform[4] = 0.0f - pos[0] * planeCoeff[1];
+            cubeXform[8] = 0.0f - pos[0] * planeCoeff[2];
+            cubeXform[12] = 0.0f - pos[0] * planeCoeff[3];
+
+            // Second column
+            cubeXform[1] = 0.0f - pos[1] * planeCoeff[0];
+            cubeXform[5] = dot - pos[1] * planeCoeff[1];
+            cubeXform[9] = 0.0f - pos[1] * planeCoeff[2];
+            cubeXform[13] = 0.0f - pos[1] * planeCoeff[3];
+
+            // Third Column
+            cubeXform[2] = 0.0f - pos[2] * planeCoeff[0];
+            cubeXform[6] = 0.0f - pos[2] * planeCoeff[1];
+            cubeXform[10] = dot - pos[2] * planeCoeff[2];
+            cubeXform[14] = 0.0f - pos[2] * planeCoeff[3];
+
+            // Fourth Column
+            cubeXform[3] = 0.0f - pos[3] * planeCoeff[0];
+            cubeXform[7] = 0.0f - pos[3] * planeCoeff[1];
+            cubeXform[11] = 0.0f - pos[3] * planeCoeff[2];
+            cubeXform[15] = dot - pos[3] * planeCoeff[3];
+        }
+
+        private void calcNormal(float[,] v, float[] outp)
+        {
+            float[] v1 = new float[3];
+            float[] v2 = new float[3];
+
+            // Calculate two vectors from the three points
+            v1[x] = v[0, x] - v[1, x];
+            v1[y] = v[0, y] - v[1, y];
+            v1[z] = v[0, z] - v[1, z];
+
+            v2[x] = v[1, x] - v[2, x];
+            v2[y] = v[1, y] - v[2, y];
+            v2[z] = v[1, z] - v[2, z];
+
+            // Take the cross product of the two vectors to get
+            // the normal vector which will be stored in out
+            outp[x] = v1[y] * v2[z] - v1[z] * v2[y];
+            outp[y] = v1[z] * v2[x] - v1[x] * v2[z];
+            outp[z] = v1[x] * v2[y] - v1[y] * v2[x];
+
+            // Normalize the vector (shorten length to one)
+            ReduceToUnit(outp);
+        }
+
+        private void ReduceToUnit(float[] vector)
+        {
+            float length;
+
+            // Calculate the length of the vector		
+            length = (float)Math.Sqrt((vector[0] * vector[0]) +
+                                (vector[1] * vector[1]) +
+                                (vector[2] * vector[2]));
+
+            // Keep the program from blowing up by providing an exceptable
+            // value for vectors that may calculated too close to zero.
+            if (length == 0.0f)
+                length = 1.0f;
+
+            // Dividing each element by the length will result in a
+            // unit normal vector.
+            vector[0] /= length;
+            vector[1] /= length;
+            vector[2] /= length;
+        }*/
     }
 
 }
